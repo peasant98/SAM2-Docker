9203ba97551f870b877dae8747499a7f67160adf
Matt Strong <mast4878@colorado.edu>
2024-08-30 01:56:40 -0700

    working example and steps

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ![image](https://github.com/user-attachments/assets/7911d7b8-72a7-4c90-9da6-7a867b0136f8)
 
 
-Segment Anything in Docker. A simple, easy to use Docker image for Meta's SAM2 with GUI support for displaying figures. Built on top of the SAM2 repo: https://github.com/facebookresearch/segment-anything-2
+Segment Anything 2 in Docker. A simple, easy to use Docker image for Meta's SAM2 with GUI support for displaying figures, images, and masks. Built on top of the SAM2 repo: https://github.com/facebookresearch/segment-anything-2
 
 
 ## Quickstart
@@ -21,7 +21,7 @@ sudo apt-get install -y nvidia-docker2
 sudo systemctl restart docker
 ```
 
-To get the SAM2 Docker image up and running, you can run:
+To get the SAM2 Docker image up and running, you can run (for NVIDIA GPUs that support at least CUDA 12.6)
 
 ```bash
 sudo usermod -aG docker $USER
@@ -29,7 +29,27 @@ newgrp docker
 docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix  -e DISPLAY=$DISPLAY --gpus all peasant98/sam2:latest bash
 ```
 
+We have a CUDA 12.1 docker image too, which can be run as follows:
+
+```bash
+docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix  -e DISPLAY=$DISPLAY --gpus all peasant98/sam2:cuda-12.1 bash
+```
+
 From this shell, you can run SAM2, as well as display plots and images.
+
+## Running the Example
+
+To check SAM2 is working within the container, we have an example in `examples/image_predictor.py` to test the image mask generation. To run:
+
+```bash
+# mount this repo, which is assumed to be in the current directory
+docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix  -v `pwd`/SAM2-Docker:/home/user/SAM2-Docker -e DISPLAY=$DISPLAY --gpus all peasant98/sam2:cuda-12.1 bash
+
+# in the container!
+cd SAM2-Docker/
+python3 examples/image_predictor.py
+
+```
 
 ## Building and Running Locally
 
---
+++ examples/image_predictor.py
@@ -0,0 +1,131 @@
+"""
+Copied script 
+from https://github.com/facebookresearch/segment-anything-2/blob/main/notebooks/image_predictor_example.ipynb
+SAM2 example script -- use this to test out the Docker image.
+"""
+
+import os
+# if using Apple MPS, fall back to CPU for unsupported ops
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+def show_mask(mask, ax, random_color=False, borders = True):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask = mask.astype(np.uint8)
+    mask_image =  mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    if borders:
+        import cv2
+        contours, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+        # Try to smooth contours
+        contours = [cv2.approxPolyDP(contour, epsilon=0.01, closed=True) for contour in contours]
+        mask_image = cv2.drawContours(mask_image, contours, -1, (1, 1, 1, 0.5), thickness=2) 
+    ax.imshow(mask_image)
+
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
+
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))    
+
+def show_masks(image, masks, scores, point_coords=None, box_coords=None, input_labels=None, borders=True):
+    for i, (mask, score) in enumerate(zip(masks, scores)):
+        plt.figure(figsize=(10, 10))
+        plt.imshow(image)
+        show_mask(mask, plt.gca(), borders=borders)
+        if point_coords is not None:
+            assert input_labels is not None
+            show_points(point_coords, input_labels, plt.gca())
+        if box_coords is not None:
+            # boxes
+            show_box(box_coords, plt.gca())
+        if len(scores) > 1:
+            plt.title(f"Mask {i+1}, Score: {score:.3f}", fontsize=18)
+        plt.axis('off')
+        plt.show()
+
+
+if __name__ == '__main__':
+    # select the device for computation
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+    print(f"using device: {device}")
+
+    if device.type == "cuda":
+        # use bfloat16 for the entire notebook
+        torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+        # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+        if torch.cuda.get_device_properties(0).major >= 8:
+            torch.backends.cuda.matmul.allow_tf32 = True
+            torch.backends.cudnn.allow_tf32 = True
+    elif device.type == "mps":
+        print(
+            "\nSupport for MPS devices is preliminary. SAM 2 is trained with CUDA and might "
+            "give numerically different outputs and sometimes degraded performance on MPS. "
+            "See e.g. https://github.com/pytorch/pytorch/issues/84936 for a discussion."
+        )
+
+    np.random.seed(3)
+
+    image = Image.open('examples/images/truck.jpg')
+    image = np.array(image.convert("RGB"))
+
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image)
+    plt.axis('on')
+    plt.show()
+
+
+    # lazy loading of sam2 imports
+    import sam2
+    from sam2.build_sam import build_sam2
+    from sam2.sam2_image_predictor import SAM2ImagePredictor
+
+    root_path = os.path.dirname(sam2.__file__)
+    root_path = os.path.dirname(root_path)
+
+    sam2_checkpoint = f"{root_path}/checkpoints/sam2_hiera_large.pt"
+    model_cfg = "sam2_hiera_l.yaml"
+
+    sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=device)
+
+    predictor = SAM2ImagePredictor(sam2_model)
+
+    predictor.set_image(image)
+
+    input_point = np.array([[500, 375]])
+    input_label = np.array([1])
+
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image)
+    show_points(input_point, input_label, plt.gca())
+    plt.axis('on')
+    plt.show() 
+
+    masks, scores, logits = predictor.predict(
+        point_coords=input_point,
+        point_labels=input_label,
+        multimask_output=True,
+    )
+    sorted_ind = np.argsort(scores)[::-1]
+    masks = masks[sorted_ind]
+    scores = scores[sorted_ind]
+    logits = logits[sorted_ind] 
+
+    show_masks(image, masks, scores, point_coords=input_point, input_labels=input_label, borders=True)
